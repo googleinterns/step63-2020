3c97f81e79290da2d59ed620b3c9f10de882d936
Erys Dennis <erysd.school@gmail.com>
2020-07-17 17:54:06 +0000

    fix file name

--- portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -28,7 +28,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/login")
-public class LoginStatusServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
